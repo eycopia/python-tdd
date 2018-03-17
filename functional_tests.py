d23e1ab87fc5d48f6f758bd15e27c9052719911e
Jorge Copia <eycopia@gmail.com>
2018-03-17 13:45:22 -0500

    Refactor duplications peacock

--- functional_tests.py
+++ functional_tests.py
@@ -33,17 +33,25 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "new to-do item did not appear in table"
-        )
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     f"new to-do item did not appear in table. Contents were:\n{table.text}"
+        # )
+
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
         # There is still a text box inviting her to add another item.
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
+        # self.fail('Finish the test!')
         # The page updates again, and now shows both items on her list
 
         # Edith wonders wheter the site will remember her list. Then she sees
