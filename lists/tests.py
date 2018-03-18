c5d27b915273f66858b6fd46fc4c77f28ecb8cb5
Jorge Copia <eycopia@gmail.com>
2018-03-18 16:17:09 -0500

    Pasa test unitarios

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -25,24 +25,34 @@ class HomePageTest(TestCase):
 
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The firsts (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
-        first_saved_item = saved_items[0].text
-        second_saved_item = saved_items[1].text
-        self.assertEqual(first_saved_item, 'The firsts (ever) list item')
-        self.assertEqual(second_saved_item, 'Item the second')
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The firsts (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
@@ -51,7 +61,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        pass
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list = list_)
+        Item.objects.create(text='itemey 2', list = list_)
 
 class NewListTest(TestCase):
 
