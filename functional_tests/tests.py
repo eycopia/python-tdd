ad2d03b703144fa8407942c6833916c45190d8d0
Jorge Copia <eycopia@gmail.com>
2018-03-18 12:38:10 -0500

    camino a una vista por usuario

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -42,9 +42,9 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         self.browser.get(self.live_server_url)
@@ -66,7 +66,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         #Francis visits the home page. There is no sign of Edith list
         self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element_by_tag_name('body')
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
