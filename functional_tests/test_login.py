124b9028589580685adad711bd7a93a8f85f3eb2
Jorge Copia <eycopia@gmail.com>
2018-05-08 19:47:27 -0500

    FT for login via email

---
+++ functional_tests/test_login.py
@@ -0,0 +1,45 @@
+from django.core import mail
+from selenium.webdriver.common.keys import Keys
+import re
+
+from .base import FunctionalTest
+
+TEST_EMAIL = 'gozusar@example.com'
+SUBJECT = 'Your login link for Superlists'
+
+class LoginTest(FunctionalTest):
+
+    def test_can_get_email_link_to_log_in(self):
+        #edith require log in
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
+
+        #appear a message
+        self.wait_for(lambda: self.assertIn(
+            'Check your email',
+            self.browser.find_element_by_name('body').text
+        ))
+
+        #she check her email
+        email = mail.outbox[0]
+        self.assertIn(TEST_EMAIL, email.to)
+        self.assertEqual(email.subject, SUBJECT)
+
+        #it has a url link in it
+        self.assertIn('Use this link to log in', email.body)
+        url_search = re.search(r'http://.+/.+$', email.body)
+        if not url_search:
+            self.fail(f'Could not find url in email body:\n{email.body}')
+        url = url_search.group[0]
+        self.assertIn(self.live_server_url, url)
+
+        #she clicks it
+        self.browser.get(url)
+
+        #she is logged in!
+        self.wait_for(
+            lambda: self.browser.find_element_by_name('Log out')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(TEST_EMAIL, navbar.text)
