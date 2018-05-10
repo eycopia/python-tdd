e192653b5c3f60379a0d6d180861540fd796f14b
Jorge Copia <eycopia@gmail.com>
2018-05-09 19:42:45 -0500

    mocks init

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -125,7 +125,7 @@ class ListViewTest(TestCase):
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
-        
+
         response = self.client.post(
             f'/lists/{list1.id}/',
             data={'text': 'textey'}
